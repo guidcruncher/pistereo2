acfd5f3bf2d4de3db571fdc354521b7e03c9b3a8
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-05-27 06:57:46 +0100

    mix

---
+++ client/src/components/Mixer.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup></script>
+<script lang="ts">
+import { PlayerService } from '@/services/player.service'
+import { on, emit, off } from '@/composables/useeventbus'
+
+export default {
+  name: 'Mixer',
+  props: {},
+  data() {
+    return {}
+  },
+  mounted() {
+    const playerService = new PlayerService()
+  },
+  beforeUnmount() {},
+  methods: {},
+}
+</script>
+<template></template>
+<style></style>
