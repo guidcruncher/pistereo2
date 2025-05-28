8104f0c1ac0a175715415ddc44ca3a2be2cc2094
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-05-28 01:33:14 +0100

    fixed ui

--- client/src/components/Mixer.vue
+++ client/src/components/Mixer.vue
@@ -7,10 +7,10 @@ export default {
   name: 'Mixer',
   props: {},
   data() {
-    return { mixer: {} as any, hasData: false, mode: 'advanced' }
+    return { mixer: {} as any, hasData: false, mode: 'simple' }
   },
   mounted() {
-    const playerService = new PlayerService()
+    const playerService = new PlayerService()ttt"sßssssssxx%-•%-%
     this.loadMixer()
   },
   beforeUnmount() {},
@@ -46,57 +46,59 @@ export default {
 }
 </script>
 <template>
-  <v-slide-group show-arrows v-if="hasData">
-    <v-slide-group-item
-      v-for="item in mixer.frequencies"
-      :key="item"
-      v-slot="{ isSelected, toggle }"
-      :value="item"
-    >
-      <div v-if="mode == 'simple'">
-        <v-slider
-          v-model="item.value"
-          direction="vertical"
-          :min="item.min"
-          :max="item.max"
-          :step="item.steps"
-          @end="setEqualiser(item, -1)"
-        >
-          <template #label>
-            <div class="text-caption">{{ item.title }} {{ item.value }}</div>
-          </template>
-        </v-slider>
-      </div>
-      <div v-else>
-        {{ item.channels[0].name }}
-        <v-slider
-          v-model="item.channels[0].value"
-          direction="vertical"
-          :min="item.min"
-          :max="item.max"
-          :step="item.steps"
-          @end="setEqualiser(item, 0)"
-        >
-          <template #label>
-            <div class="text-caption">{{ item.title }} {{ item.value }}</div>
-          </template>
-        </v-slider>
+  <v-card>
+    <v-slide-group show-arrows v-if="hasData">
+      <v-slide-group-item
+        v-for="item in mixer.frequencies"
+        :key="item"
+        v-slot="{ isSelected, toggle }"
+        :value="item"
+      >
+        <div v-if="mode == 'simple'">
+          <v-slider
+            v-model="item.value"
+            direction="vertical"
+            :min="item.min"
+            :max="item.max"
+            :step="item.steps"
+            @end="setEqualiser(item, -1)"
+          >
+            <template #label>
+              <div class="text-caption">{{ item.title }} {{ item.value }}</div>
+            </template>
+          </v-slider>
+        </div>
+        <div v-else>
+          {{ item.channels[0].name }}
+          <v-slider
+            v-model="item.channels[0].value"
+            direction="vertical"
+            :min="item.min"
+            :max="item.max"
+            :step="item.steps"
+            @end="setEqualiser(item, 0)"
+          >
+            <template #label>
+              <div class="text-caption">{{ item.title }} {{ item.value }}</div>
+            </template>
+          </v-slider>
 
-        {{ item.channels[1].name }}
-        <v-slider
-          v-model="item.channels[1].value"
-          direction="vertical"
-          :min="item.min"
-          :max="item.max"
-          :step="item.steps"
-          @end="setEqualiser(item, 1)"
-        >
-          <template #label>
-            <div class="text-caption">{{ item.title }} {{ item.value }}</div>
-          </template>
-        </v-slider>
-      </div>
-    </v-slide-group-item>
-  </v-slide-group>
+          {{ item.channels[1].name }}
+          <v-slider
+            v-model="item.channels[1].value"
+            direction="vertical"
+            :min="item.min"
+            :max="item.max"
+            :step="item.steps"
+            @end="setEqualiser(item, 1)"
+          >
+            <template #label>
+              <div class="text-caption">{{ item.title }} {{ item.value }}</div>
+            </template>
+          </v-slider>
+        </div>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-card>
 </template>
 <style></style>
