10e9c89f992c5e5fab5593f608a10fa210a28308
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-05-29 14:13:31 +0100

    Build containers

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker buildx build -f Dockerfile-bullseye -t guidcruncher/pistereo2:bullseye-latest --pull --push .
+
+docker buildx build -f Dockerfile -t guidcruncher/pistereo2:latest --pull --push .
