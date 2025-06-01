ddf5ad5472b7e9966b002724ba2191f257c42458
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-06-01 01:46:23 +0100

    Dockerfile revisions

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker buildx build -f Dockerfile-bullseye -t guidcruncher/pistereo2:bullseye-latest --pull --push .
-
-docker buildx build -f Dockerfile -t guidcruncher/pistereo2:latest --pull --push .
-
-# docker buildx build -f Dockerfile-media -t guidcruncher/pistereo2:mediaserver --pull --push .
-
---
+++ dockerfiles/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker buildx build -f ./dockerfiles/Dockerfile-bullseye -t guidcruncher/pistereo2:bullseye-latest -t guidcruncher/pistereo2:latest --pull --push .
+
+docker buildx build -f ./dockerfiles/Dockerfile-alpine -t guidcruncher/pistereo2:alpine-latest --pull --push .
+
+# docker buildx build -f ./dockerfiles/Dockerfile-mediaserver -t guidcruncher/pistereo2:mediaserver --pull --push .
+
