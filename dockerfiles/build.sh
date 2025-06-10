045116d0b1be59c92229d353d8253ae6e51677bc
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-06-10 06:43:40 +0100

    fix

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+arch="$(uname -m)"
+if [ == "aarch64" ]; then
+docker buildx build -f ./dockerfiles/Dockerfile-bullseye -t guidcruncher/pistereo2:bullseye-latest -t guidcruncher/pistereo2:latest --pull --push .
 
-docker buildx build -f ./dockerfiles/Dockerfile-bullseye -t guidcruncher/pistereo2:bullseye-latest -t guidcruncher/pistereo2:latest --platform linux/arm64y --pull --push .
+# docker buildx build -f ./dockerfiles/Dockerfile-alpine -t guidcruncher/pistereo2:alpine-latest --pull --push .
+
+# docker buildx build -f ./dockerfiles/Dockerfile-mediaserver -t guidcruncher/pistereo2:mediaserver --pull --push .
+else
+docker buildx build -f ./dockerfiles/Dockerfile-bullseye -t guidcruncher/pistereo2:bullseye-latest -t guidcruncher/pistereo2:latest --platform linux/arm64 --pull --push .
 
 # docker buildx build -f ./dockerfiles/Dockerfile-alpine -t guidcruncher/pistereo2:alpine-latest --pull --push --platform linux/arm64 .
 
 # docker buildx build -f ./dockerfiles/Dockerfile-mediaserver -t guidcruncher/pistereo2:mediaserver --pull --push --platform linux/arm64 .
 
+fi
